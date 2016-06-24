a924a2fb2442aaf361fd7f6b749d5827822c1b66
Jonas Zaddach <jonas@zaddach.org>
2016-06-25 00:05:26 +0200

    Fix missing symbols through stubbing

--- libqemu/stubs.c
+++ libqemu/stubs.c
@@ -11,16 +11,6 @@ const char *qemu_uname_release;
 /***********************************************************/
 /* Helper routines for implementing atomic operations.  */
 
-/* To implement exclusive operations we force all cpus to syncronise.
-   We don't require a full sync, only that no cpus are executing guest code.
-   The alternative is to map target atomic ops onto host equivalents,
-   which requires quite a lot of per host/target work.  */
-static pthread_mutex_t cpu_list_mutex = PTHREAD_MUTEX_INITIALIZER;
-static pthread_mutex_t exclusive_lock = PTHREAD_MUTEX_INITIALIZER;
-static pthread_cond_t exclusive_cond = PTHREAD_COND_INITIALIZER;
-static pthread_cond_t exclusive_resume = PTHREAD_COND_INITIALIZER;
-static int pending_cpus;
-
 /* Make sure everything is in a consistent state for calling fork().  */
 void fork_start(void)
 {
@@ -120,4 +110,8 @@ CPUArchState *cpu_copy(CPUArchState *env) {return NULL;}
 void stop_all_tasks(void) {}
 void print_syscall_ret(int num, abi_long ret) {}
 void gemu_log(const char *fmt, ...) {}
+
+abi_long do_brk(abi_ulong new_brk) {
+    return 0;
+}
         
